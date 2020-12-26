1a0df9f5d2e6ba64b067f81e8d7a19fd82eb47ac
Leroy Stav <epimeth@epimeth.net>
2020-12-25 21:09:10 -0500

    Installed SvelteKit

---
+++ AgoraSocialApp/SvelteKit/src/routes/$layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Nav from '$components/Nav.svelte';
+</script>
+
+<main>
+    <Nav />
+    <slot></slot>
+</main>
+
+<style lang="scss">
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+</style>
---
+++ AgoraSocialApp/SvelteKit/src/routes/index.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Counter from '$components/Counter.svelte';
+</script>
+
+	<h1>Hello world!</h1>
+
+	<Counter/>
+	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+
+<style lang="scss">
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 100;
+		line-height: 1.1;
+		margin: 4rem auto;
+		max-width: 14rem;
+	}
+
+	p {
+		max-width: 14rem;
+		margin: 2rem auto;
+		line-height: 1.35;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			max-width: none;
+		}
+
+		p {
+			max-width: none;
+		}
+	}
+</style>
