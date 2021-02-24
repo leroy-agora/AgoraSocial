a4dd5cd93457cd06ae500126ad3de89a4a136d59
Jeremy Shankle <jpshankle@gmail.com>
2021-02-24 14:22:38 -0500

    don't extend config for now to avoid changing deps

--- AgoraSocialApp/SvelteKit/snowpack.config.cjs
+++ AgoraSocialApp/SvelteKit/snowpack.config.cjs
@@ -1,17 +1,39 @@
+const path = require('path');
+const pkg = require(path.join(process.cwd(), 'package.json'));
+
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-	extends: '@sveltejs/snowpack-config',
+	packageOptions: {
+		// always include Svelte in your project
+		knownEntrypoints: ['svelte'],
+		// ignore `import fs from 'fs'` etc
+		external: require('module').builtinModules
+	},
 	plugins: [
-		...require('@sveltejs/snowpack-config').plugins,
-		'@snowpack/plugin-typescript'
+		[
+			'@snowpack/plugin-svelte',
+			{
+				compilerOptions: {
+					hydratable: true
+				},
+				configFilePath: 'svelte.config.cjs'
+			},
+      '@snowpack/plugin-typescript'
+		]
 	],
+	devOptions: {
+		open: 'none',
+		output: 'stream'
+	},
+	buildOptions: {
+		sourcemap: true
+	},
 	mount: {
-		'src/components': '/_components'
+    'src/components': '/_components',
+		'.svelte/assets': `/${process.env.SVELTE_KIT_APP_DIR}/assets`
 	},
 	alias: {
-		$components: './src/components'
-	},
-	packageOptions: {
-		knownEntrypoints: ['rxjs/operators'],
-	  },	
+    $components: './src/components',
+		$app: './.svelte/assets/runtime/app'
+	}
 };
