29fae38caee73dc23bdceff90140a8b812c5fcd8
Jeremy <jeremy@getagora.co>
2021-03-16 17:53:45 -0400

    tailwind css typography

--- AgoraSocialApp/SvelteKit/src/routes/login/index.svelte
+++ AgoraSocialApp/SvelteKit/src/routes/login/index.svelte
@@ -6,8 +6,8 @@
   // TODO remove hardcoded redirectUrl
 	const login = () => authService.signIn({ redirectUrl: '/app' });
 </script>
-<div class="container sm:mx-auto">
-<h1>Welcome to Agora!</h1>
+<div class="prose prose-sm text-center">
+<h3>Welcome to Agora!</h3>
 <p>
   Your subscription with <b>{$page.params.conversation}</b> will give you access to a community<br />
   of fellow subscribers and a platform to explore more Creator content across the Agora.
--- AgoraSocialApp/SvelteKit/tailwind.config.cjs
+++ AgoraSocialApp/SvelteKit/tailwind.config.cjs
@@ -25,5 +25,7 @@ module.exports = {
 	variants: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [
+    require('@tailwindcss/typography')
+  ]
 };
