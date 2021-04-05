34e3239bc4bfbba50baccea97a16bd678fc04465
Leroy Stav <epimeth@epimeth.net>
2021-04-05 03:34:00 -0400

    Raven Identity with Google Login added

---
+++ AgoraSocialApp/Filters/RavenSaveChangesAsyncFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Raven.Client.Documents.Session;
+using System.Threading.Tasks;
+
+namespace AgoraSocialApp.Filters
+{
+    /// <summary>
+    /// Razor Pages filter that saves any changes after the action completes.
+    /// </summary>
+    public class RavenSaveChangesAsyncFilter : IAsyncPageFilter
+    {
+        private readonly IAsyncDocumentSession dbSession;
+
+        public RavenSaveChangesAsyncFilter(IAsyncDocumentSession dbSession)
+        {
+            this.dbSession = dbSession;
+        }
+
+        public async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            var result = await next.Invoke();
+
+            // If there was no exception, and the action wasn't cancelled, save changes.
+            if (result.Exception == null && !result.Canceled)
+            {
+                await dbSession.SaveChangesAsync();
+            }
+        }
+    }
+}
--- AgoraSocialApp/Program.cs
+++ AgoraSocialApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,10 @@ namespace AgoraSocialApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
