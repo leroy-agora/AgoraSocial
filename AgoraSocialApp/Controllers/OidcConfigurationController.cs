e992d7549538a8e2aacf4baba86e03051b93d684
Leroy Stav <epimeth@epimeth.net>
2021-04-16 05:12:04 -0400

    fixed config controller

--- AgoraSocialApp/Controllers/OidcConfigurationController.cs
+++ AgoraSocialApp/Controllers/OidcConfigurationController.cs
@@ -1,26 +1,47 @@
-﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+﻿using IdentityServer4.Extensions;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AgoraSocialApp.Controllers
 {
     public class OidcConfigurationController : Controller
     {
         private readonly ILogger<OidcConfigurationController> _logger;
+        private readonly IClientStore _clientStore;
 
-        public OidcConfigurationController(IClientRequestParametersProvider clientRequestParametersProvider, ILogger<OidcConfigurationController> logger)
+        private readonly IResourceStore _resourceStore;
+
+        public OidcConfigurationController(ILogger<OidcConfigurationController> logger, IClientStore clientStore, IResourceStore resourceStore)
         {
-            ClientRequestParametersProvider = clientRequestParametersProvider;
             _logger = logger;
+            _clientStore = clientStore;
+            _resourceStore = resourceStore;
+
         }
 
-        public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
-
         [HttpGet("_configuration/{clientId}")]
-        public IActionResult GetClientRequestParameters([FromRoute] string clientId)
+        public async Task<IActionResult> GetClientRequestParameters([FromRoute] string clientId)
         {
-            var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
-            return Ok(parameters);
+            var context = HttpContext;
+            var authority = context.GetIdentityServerIssuerUri();
+            var client = await _clientStore.FindClientByIdAsync(clientId);
+
+            var resources = await _resourceStore.GetAllResourcesAsync();
+
+            return Ok(new Dictionary<string, string>
+            {
+                ["authority"] = authority,
+                ["client_id"] = client.ClientId,
+                ["redirect_uri"] = client.RedirectUris.FirstOrDefault(),
+                ["post_logout_redirect_uri"] = client.PostLogoutRedirectUris.FirstOrDefault(),
+                ["response_type"] = "code",
+                ["scope"] = string.Join(" ", client.AllowedScopes)
+            });
         }
     }
 }
