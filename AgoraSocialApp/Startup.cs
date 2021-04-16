ff287b3cf6197eef493745414666ce1d27a851ab
Leroy Stav <epimeth@epimeth.net>
2021-04-16 05:12:04 -0400

    fixed config controller

--- AgoraSocialApp/Startup.cs
+++ AgoraSocialApp/Startup.cs
@@ -69,6 +69,7 @@ namespace AgoraSocialApp
                 .AddApiResources()
                 //.AddClients()
                 .AddClientStore<RavenDBClientStore>()
+                //.AddResourceStore<AgoraRavenDBResourceStore>()
                 .AddCorsPolicyService<CorsPolicyService>()
                 .AddSigningCredentials();
 
